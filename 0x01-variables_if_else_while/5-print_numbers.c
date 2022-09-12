89252643c1d22cadf781700f3b63f26db996dbc9
queenbuuri <buurilorna160@gmail.com>
2022-09-12 15:49:29 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,11 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {	int d = 48;
 	while (d < 58)
-	{	
-		putchar(d);
+	{	putchar(d);
 		d++;
-	}	
-		putchar('\n');
-		return (0);
+	}	putchar('\n');
+	return (0);
 }
